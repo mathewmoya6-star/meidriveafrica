d55560bfd145cccdf050a62ccb9e4bde4c19876b
mathewmoya6-star <mathewmoya6@gmail.com>
2026-05-17 19:40:54 +0300

    Create sw.js

---
+++ public /sw.js
@@ -0,0 +1,10 @@
+const CACHE_NAME = 'meidrive-v1';
+const urlsToCache = ['/', '/index.html', '/login.html', '/register.html', '/dashboard.html', '/course.html', '/offline.html'];
+
+self.addEventListener('install', event => {
+    event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(caches.match(event.request).then(response => response || fetch(event.request).catch(() => caches.match('/offline.html'))));
+});
